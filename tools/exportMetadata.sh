c6c4e3afc267a5ada5fae598a061521f88252da6
Edwin Joassart <edwin@blck.coffee>
2022-05-04 19:33:20 +0200

    WIP - meta & chapters

--- tools/exportMetadata.sh
+++ tools/exportMetadata.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 # https://ffmpeg.org/ffmpeg-formats.html#Metadata-1
 
+# Extract the initial metadata (technical stuffs)
 yes | ffmpeg -i output.mp4 -f ffmetadata FFMETADATAFILE.txt
 
 # To construct the chapters we need to extract the length of each videos after trim, and their title.
-
 
 # Here we set the main meta
 echo "title=hello world" >> FFMETADATAFILE.txt
 echo "artist=BMovie Team Test" >> FFMETADATAFILE.txt
 
 # then we loop over the files to get the length and create chapters
+# FIXME : find doesn't output the files in the right order, need some sorting
+# FIXME : the Length is not right yet
 
 # ffprobe -i 01-intro/intro.mp4 -v quiet -show_entries format=duration -hide_banner -of default=noprint_wrappers=1:nokey=1 
 
@@ -31,12 +33,5 @@ find . -type f -not -name '.*' -a \( -name "*.mp4" \) \
     # LASTSTART = LASTSTART + LENGTH
 ' _ '{}' \;
 
-
-echo "[CHAPTER]" >> FFMETADATAFILE.txt
-echo "TIMEBASE=1/1000" >> FFMETADATAFILE.txt
-echo "START=60001" >> FFMETADATAFILE.txt
-#chapter ends at 0:02:00
-echo "END=120000" >> FFMETADATAFILE.txt
-echo "title=chapter \#2" >> FFMETADATAFILE.txt
-
+# Then we write back the new meta into the output
 ffmpeg -i output.mp4 -i FFMETADATAFILE.txt -map_metadata 1 -codec copy output-meta.mp4
