325036c583bce6cf642fe3778a198d029da4e38e
Flynn Joffray <nucleardreamer@gmail.com>
2022-06-02 14:30:34 -0700

    adding optional branch for source checkout
    Signed-off-by: Flynn Joffray <nucleardreamer@gmail.com>

--- tools/fetchGitSource.sh
+++ tools/fetchGitSource.sh
@@ -4,17 +4,27 @@
 #
 
 echo "* Removing /files/INPUT"
-rm -rf /files/INPUT && mkdir -p /files/INPUT
-rm -rf /tmp/SOURCE_GIT_REPO
+rm -rf /files/INPUT && mkdir -p /files/INPUT;
+rm -rf /tmp/SOURCE_GIT_REPO;
 
 echo "* Cloning source repo: ${SOURCE_GIT_URL}"
-git clone --recursive $SOURCE_GIT_URL /tmp/SOURCE_GIT_REPO
+git clone --recursive $SOURCE_GIT_URL /tmp/SOURCE_GIT_REPO;
 
-echo "* Copying source to /files/INPUT"
-mv /tmp/SOURCE_GIT_REPO/videos /files/INPUT
+if [ -z "$SOURCE_GIT_BRANCH" ]
+then
+    echo "* No branch selected, using default";
+else
+    echo "* Checking out branch: ${SOURCE_GIT_BRANCH}"
+    pushd /tmp/SOURCE_GIT_REPO;
+    git checkout $SOURCE_GIT_BRANCH;
+    popd;
+fi
 
-echo "* Removing white space in file names"
+echo "* Copying source to /files/INPUT";
+mv /tmp/SOURCE_GIT_REPO/videos/* /files/INPUT;
+
+echo "* Removing white space in file names";
 find /files/INPUT -depth -name "* *" -execdir rename 's/ /_/g' "{}" \;
 
-echo "* Cleaning source repo"
-rm -rf /tmp/SOURCE_GIT_REPO
+echo "* Cleaning source repo";
+rm -rf /tmp/SOURCE_GIT_REPO;
