c94980d94d02cb670022dfdd391d77a6d177cf16
Flynn Joffray <nucleardreamer@gmail.com>
2022-06-02 13:56:14 -0700

    renaming input files with no spaces
    Signed-off-by: Flynn Joffray <nucleardreamer@gmail.com>

--- tools/fetchGitSource.sh
+++ tools/fetchGitSource.sh
@@ -4,7 +4,7 @@
 #
 
 echo "* Removing /files/INPUT"
-rm -rf /files/INPUT
+rm -rf /files/INPUT && mkdir -p /files/INPUT
 rm -rf /tmp/SOURCE_GIT_REPO
 
 echo "* Cloning source repo: ${SOURCE_GIT_URL}"
@@ -13,5 +13,8 @@ git clone --recursive $SOURCE_GIT_URL /tmp/SOURCE_GIT_REPO
 echo "* Copying source to /files/INPUT"
 mv /tmp/SOURCE_GIT_REPO/videos /files/INPUT
 
+echo "* Removing white space in file names"
+find /files/INPUT -depth -name "* *" -execdir rename 's/ /_/g' "{}" \;
+
 echo "* Cleaning source repo"
-rm -rf /tmp/video_src/
+rm -rf /tmp/SOURCE_GIT_REPO
